97bc9975bd6fb732a4dbd996c157b3f889d567d9
Tasnimul Hasan <thtauhid.99@gmail.com>
2017-04-23 00:52:45 +0600

    Update index.php

--- index.php
+++ index.php
@@ -4,19 +4,25 @@
     <meta charset="utf-8">
     <title>Find And Replace</title>
   </head>
-  <body>
-    <?php
-        error_reporting(0);
-        $find = $_POST['find'];
-        $replace = $_POST['replace'];
-        $textarea = $_POST['textarea'];
-        $new_text = str_ireplace($find, $replace, $textarea);
-    ?>
+  <body style="text-align: center;">
     <form class="" action="index.php" method="post">
-      <textarea name="textarea" rows="8" cols="80" placeholder="Type your text here"><?php echo $new_text; ?></textarea><br>
+      <textarea name="textarea" rows="8" cols="80" placeholder="Input your text here..."></textarea><br>
       <input type="text" name="find" value="" placeholder="What to find?"><br>
       <input type="text" name="replace" value="" placeholder="What to replace?"><br>
       <input type="submit" name="submit" value="Submit">
     </form>
+
+    <br><br><hr><br><br>
+
+    <?php
+        if (isset($_POST['find']) && isset($_POST['replace']) && isset($_POST['textarea'])) {
+          $find = $_POST['find'];
+          $replace = $_POST['replace'];
+          $textarea = $_POST['textarea'];
+          $new_text = str_ireplace($find, $replace, $textarea);
+          echo "<textarea name='textarea' rows='8' cols='80' placeholder='You get your output here....'>$new_text</textarea><br>";
+
+        }
+    ?>
   </body>
 </html>
